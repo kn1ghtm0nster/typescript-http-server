eb4ab5901bc2822c08fb4e54a54126ca2b273a2d
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-07-10 14:13:14 -0500

    CH1-L5: add starting listener logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
+const PORT = 8080;
 
-app.listen(8080);
+app.use(express.static('.'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
