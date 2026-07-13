cd70956275a75abab3358310677a5a641fd8a391
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-07-13 14:46:19 -0500

    CH4-L3: refactor code to use express json middleware

--- src/handlers.ts
+++ src/handlers.ts
@@ -36,30 +36,26 @@ export const handlerValidateChirp = (
   req: Request,
   res: Response
 ): Promise<void> => {
-  let body = '';
+  type parameters = {
+    body: string;
+  };
+  try {
+    let body: parameters = req.body;
 
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-
-  req.on('end', () => {
-    try {
-      const parsedBody = JSON.parse(body);
-      if (parsedBody['body'].length >= 140) {
-        res.status(400).send({
-          error: 'Chirp is too long',
-        });
-      } else {
-        res.status(200).send({
-          valid: true,
-        });
-      }
-    } catch (error) {
+    if (body.body.length > 140) {
       res.status(400).send({
-        error: 'Something went wrong',
+        error: 'Chirp is too long',
+      });
+    } else {
+      res.status(200).send({
+        valid: true,
       });
     }
-  });
+  } catch (error) {
+    res.status(400).send({
+      error: 'Something went wrong',
+    });
+  }
 
   return Promise.resolve();
 };
