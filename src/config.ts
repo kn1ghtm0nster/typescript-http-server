54eb5c3fe10e03ed57b596a61f432cdae330c988
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-07-18 11:52:02 -0500

    CH6-L3: add starting drizzle queries | update config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,14 @@
+import { envOrThrow } from './utils/env.helpers.js';
+
+// load environment variables from .env file
+process.loadEnvFile('.env');
+
 type APIConfig = {
   fileserverHits: number;
+  dbUrl: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbUrl: envOrThrow(process.env.DB_URL || 'DB_URL'),
 };
